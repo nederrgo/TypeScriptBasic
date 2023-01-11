944d86f7720c4bbc0066d63ca340151360b04006
nederrgo <neder@rgoarchitects.com>
2023-01-11 20:31:16 +0200

    neders first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var _a, _b;
+var arrayOfNum = [];
+(_a = document.getElementById("addNum")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    var input = document.getElementById('add');
+    var value = input === null || input === void 0 ? void 0 : input.value;
+    arrayOfNum.push(Number(value));
+});
+(_b = document.getElementById("submit")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function () {
+    console.log(getAmountOfPositive(arrayOfNum));
+});
+function getAverage(array) {
+    var sum = 0;
+    array.forEach(function (element) {
+        sum += Number(element);
+    });
+    return sum / array.length;
+}
+function getAmountOfPositive(array) {
+    var positiveNumbers = 0;
+    array.forEach(function (element) {
+        if (Number(element) > 0) {
+            positiveNumbers++;
+        }
+    });
+    return positiveNumbers;
+}
---
+++ index.ts
@@ -0,0 +1,25 @@
+let arrayOfNum :number[]= [];
+document.getElementById("addNum")?.addEventListener("click",()=>{
+    const input = document.getElementById('add') as HTMLInputElement | null;
+    const value = input?.value;
+    arrayOfNum.push(Number(value));
+});
+document.getElementById("submit")?.addEventListener("click",()=>{
+    console.log(getAmountOfPositive(arrayOfNum));
+});
+function getAverage(array :number[]) :number{
+    let sum=0;
+    array.forEach(element => {
+        sum+=Number(element);
+    });
+    return sum/array.length;
+}
+function getAmountOfPositive(array :number[]):number{
+    let positiveNumbers=0;
+    array.forEach(element => {
+        if(Number(element)>0){
+        positiveNumbers++;
+        }
+    });
+    return positiveNumbers;
+}
